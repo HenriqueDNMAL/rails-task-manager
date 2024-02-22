5b7322979c655a8804c2db11b4c7753b335175a5
henrique lopes <henrique.0.lopes@hotmail.com>
2024-02-22 13:10:05 -0300

    resolvido

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,27 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # # See ALL tasks
+  get 'tasks', to: 'tasks#index'
+
+  # # Get task info with form
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+
+  # # Create a task
+  post 'tasks', to: 'tasks#create'
+
+  # # See ONE task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # Get task info to update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # # Update a task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
